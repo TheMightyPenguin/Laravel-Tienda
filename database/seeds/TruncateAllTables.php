9c024b1241aa43e6c7672e569a873f28181505a1
victor tortolero <victormtortolero@gmail.com>
2016-06-19 07:47:26 -0400

    Add seeder to truncate all tables

---
+++ database/seeds/TruncateAllTables.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TruncateAllTables extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+        DB::statement("SET foreign_key_checks=0");
+        $tableNames = Schema::getConnection()->getDoctrineSchemaManager()->listTableNames();
+
+        foreach($tableNames as $name) {
+            if($name == 'migrations')
+                continue;
+            DB::table($name)->truncate();
+        }
+        DB::statement("SET foreign_key_checks=1");
+    }
+}
